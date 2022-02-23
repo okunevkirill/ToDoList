5c049c3a859063c73cbaae4576e2205e2bc1cc2b
ok_kir <clicktmin@gmail.com>
2022-02-23 21:36:32 +0400

    Added base on the task of lesson_001

--- todolist/users/models.py
+++ todolist/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """API user model"""
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    email = models.EmailField(unique=True)
