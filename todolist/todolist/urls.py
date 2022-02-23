eca238dea1d484f6b466b59bcabae838456ad387
ok_kir <clicktmin@gmail.com>
2022-02-23 16:26:58 +0400

    Initialization

---
+++ todolist/todolist/urls.py
@@ -0,0 +1,13 @@
+"""
+todolist URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # ------------------------------
+    path('api-auth/', include('rest_framework.urls')),
+    # ------------------------------
+
+]
