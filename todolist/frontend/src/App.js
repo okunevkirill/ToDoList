bbbfbf6aea2e5daa9d49814959febf993fd954b6
ok_kir <clicktmin@gmail.com>
2022-02-26 22:48:19 +0400

    Added base on the task of lesson_002

--- todolist/frontend/src/App.js
+++ todolist/frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserList from "./components/Users";
+import {TopHeading, Footer} from "./components/staticTags";
+
+// ----------------------------------------------------------------------------
+const URL = 'http://127.0.0.1:8000/'
+const URN_USERS = 'api/users/'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`${URL}${URN_USERS}`)
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }).catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TopHeading />
+                <UserList users={this.state.users} />
+                <Footer />
+            </div>
+        )
+    }
 }
 
+// ----------------------------------------------------------------------------
 export default App;
