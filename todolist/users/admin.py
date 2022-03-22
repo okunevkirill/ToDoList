4e24ec7b21a5e226a021607dbbe5b09540214dab
ok_kir <clicktmin@gmail.com>
2022-03-22 19:05:59 +0400

    Changed the display of the user in the admin panel (password caching implementation)

--- todolist/users/admin.py
+++ todolist/users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
     list_display = ('uid', 'username', 'first_name', 'last_name', 'email', 'date_joined')
     list_display_links = ('uid', 'username', 'email')
     search_fields = ('uid', 'username', 'first_name', 'last_name', 'email')
