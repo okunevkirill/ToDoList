370a80828b543e875bdd3b8125c816d3504506b9
ok_kir <clicktmin@gmail.com>
2022-03-23 02:26:45 +0400

    bugfix with missing email field in admin form

--- todolist/users/admin.py
+++ todolist/users/admin.py
@@ -6,7 +6,10 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )   # Expanding the list of fields to display in the form
+
     list_display = ('uid', 'username', 'first_name', 'last_name', 'email', 'date_joined')
     list_display_links = ('uid', 'username', 'email')
     search_fields = ('uid', 'username', 'first_name', 'last_name', 'email')
