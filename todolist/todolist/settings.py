4f6896b4643aab363851e93d9c1d19a0e120e24b
ok_kir <clicktmin@gmail.com>
2022-04-15 19:11:49 +0400

    URL correction (backend)

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -157,6 +157,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:80',
     'http://127.0.0.1:80',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 # rest framework response
